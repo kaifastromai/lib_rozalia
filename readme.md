6e649d207646b72116e61b4d9f9dcc64951b9b56
Kaifas <cs.stormindustries@outlook.com>
2022-03-21 01:04:39 -0500

    init

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Rozalia
+Rozalia is a simple tool for developing synthetic languages using a rule-based grammar system for creative writing.
+It is part of the Novella Project.
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+/*!Rozalia is a synthetic language generation engine for creative people. It allows the development of
+ *  generic rule systems and the ability to populate large dictionaries with procedural words that follow certain rules.
+ * It will likely be initially limited to language that follow Indo-European roots, but ideally more generic systems can be developed
+ *    */
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
